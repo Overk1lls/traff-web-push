05cf732d56daaeba7850858d2da07b22102fc351
Yurii Pakhota <ov3rfordream@gmail.com>
2025-05-15 11:13:14 +0300

    Add the validation global pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import appConfig, { AppConfig } from './config/app/app.config';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
 
   const logger = new Logger('NestApplication');
   const { port } = app.get<AppConfig>(appConfig.KEY);
